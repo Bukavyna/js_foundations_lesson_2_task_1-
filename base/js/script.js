e19e48244ff0fcdd932c2198dfcab57bfc4cbb4e
Taras <luchakt2@gmail.com>
2023-08-30 12:02:30 +0300

    initial

---
+++ base/js/script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+
+let count = 0;
+console.log("Count =", count);
+let testColorSun = prompt("Якого кольору сонце?");
+if(testColorSun == "Жовтого") {
+	console.log("Відповідь вірна");
+	count++;
+}
+
+console.log("Count =", count);
+let testColorGrass = prompt("Якого кольору трава?");
+if(testColorGrass == "Зеленого") {
+	console.log("Відповідь вірна");
+	count++;
+}
+
+console.log("Count =", count);
+let testCountries = prompt("Яка країна найкраща?");
+if(testCountries == "Україна") {
+	console.log("Відповідь вірна");
+	count++;
+}
+
+console.log("Count =", count);
+let testAddition = prompt("Скільки буде 2+2?");
+if(testAddition == "4") {
+	console.log("Відповідь вірна");
+	count++;
+}
+
+console.log("Count =", count);
+let testPresident = prompt("Як звуть пркзидента московії?");
+if(testPresident == "хуйло") {
+	console.log("БІНГОООО!!!! Ви молодець!!!!!!!");
+	count++;
+}
+
+console.log("Count =", count);
+let testAnimals = prompt("Яка тварина найшвидша?");
+if(testAnimals == "Гепард") {
+	console.log("Відповідь вірна");
+	count++;
+}
+
+console.log("Count =", count);
+let testlanguare = prompt("Яку мову звуть соловї`ною?");
+if(testlanguare == "Українську") {
+	console.log("Відповідь вірна");
+	count++;
+}
+
+console.log("Count =", count);
+let testColorSky = prompt("Яого кольору небо?");
+if(testColorSky == "Блакитного") {
+	console.log("Відповідь вірна");
+	count++;
+}
+
+console.log("Count =", count);
+let testNamesInsects = prompt("Як українською буде стрекоза?");
+if(testNamesInsects == "Бабка") {
+	console.log("Відповідь вірна");
+	count++;
+}
+
+console.log("Count =", count);
+let testcreative = prompt("Як українською буде креативний?");
+if(testcreative == "Творчий") {
+	console.log("Відповідь вірна");
+	count++;
+}
